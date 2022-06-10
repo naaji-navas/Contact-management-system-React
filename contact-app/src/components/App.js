054bdf21bd1ccad2b68603913679a61bafabab5b
naaji-navas <najidnavas2000@gmail.com>
2022-06-11 02:22:07 +0530

    commi

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -4,15 +4,24 @@ import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
 
  const [contacts, setContacts] = useState([]);
-
   const addContactHandler =(contact) =>{
     console.log(contact);
     setContacts([...contacts,contact]);
     
 
   }
+
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retrieveContacts) setContacts(retrieveContacts);
+},[]);
+
+  useEffect(() => {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  },[contacts]);
 
   return (
     <div className='ui container'>
